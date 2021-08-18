56da28f28856c0a68ebac4e751fbd6d8e8f15332
Erick Ontiveros <erickontiveros017gmail.com>
2021-08-18 16:42:49 -0300

    Código completo

--- js/scriptUC009-SA1-A2.js
+++ js/scriptUC009-SA1-A2.js
@@ -1,27 +1,82 @@
-function validaFormulario(){
-    //alert('Entrei no Validar Formulário');
-    var confirm = true;
+function validaFormulario() {
     var vname = document.getElementById('name').value;
+    var vNickName = document.getElementById('nickname').value;
+    var vDay = document.getElementById('day').value;
+    var vMonth = document.getElementById('month').value;
+    var vYear = document.getElementById('year').value;
+    var vTeamId = document.getElementById('team_id').value;
     var vcpf = document.getElementById('cpf');
 
-    if(vname.length>=10 && vname.length<=80){
-        confirm = true;
-    } else { 
+    var sportlist = document.getElementsByName("sport");
+    var sportlistSelected = [];
+    
+    if(sportlist[0].checked){
+        sportlistSelected.push(sportlist[0].id);
+    }
+
+    if(sportlist[1].checked){
+        sportlistSelected.push(sportlist[1].id);
+    }
+
+    if(sportlist[2].checked){
+        sportlistSelected.push(sportlist[2].id);
+    }
+
+    if(sportlist[3].checked){
+        sportlistSelected.push(sportlist[3].id);
+    }
+
+    if(sportlist[4].checked){
+        sportlistSelected.push(sportlist[4].id);
+    }
+
+    if(sportlist[5].checked){
+        sportlistSelected.push(sportlist[5].id);
+    }
+
+    if(sportlist[6].checked){
+        sportlistSelected.push(sportlist[6].id);
+    }
+
+    var confirm = false;
+
+    if (vname.length >= 10 && vname.length <= 80) {
+        if (vNickName.length >= 2 && vNickName.length <= 20) {
+            if (vDay >= 1 && vDay <= 31) {
+                if (vMonth >= 1 && vMonth <= 12) {
+                    if (vYear >= 1900 && vYear <= 2021) {
+                        if (vTeamId != "") {
+                            if (vcpf.value.length == 14 && vcpf.checkValidity()) {
+                                if(sportlistSelected.length>0){
+                                    confirm = true;
+                                } else {
+                                    confirm = false;
+                                }
+                            } else {
+                                confirm = false;
+                            }
+                        } else {
+                            confirm = false;
+                        }
+                    }
+                } else {
+                    confirm = false;
+                }
+            } else {
+                confirm = false;
+            }
+        } else {
+            confirm = false;
+        }
+    } else {
         confirm = false;
     }
 
-    if(vcpf.value.length = 14 && vcpf.checkValidity()){
-        confirm = true
-    } else {
-        confirm = false
-    }
-
-    var presposta = document.getElementById("resposta");
-    presposta.innerHTML = confirm;
+    var resposta = document.getElementById("resposta");
+    resposta.innerHTML = confirm;
 }
 
-function geraJSON(){
-    //alert('Entrei no Gera JSON');
+function geraJSON() {
     var index_obj = {
         aName: "",
         aNickname: "",
@@ -34,12 +89,44 @@ function geraJSON(){
     }
 
     index_obj.aName = document.getElementById('name').value;
-    var vJson = JSON.stringify(index_obj);
     var jresposta = document.getElementById('resposta');
-    jresposta.innerHTML = vJson;
-    
+    index_obj.aNickname = document.getElementById('nickname').value;
+    index_obj.aDay = document.getElementById('day').value;
+    index_obj.aMonth = document.getElementById('month').value;
+    index_obj.aYear = document.getElementById('year').value;
     index_obj.aCPF = document.getElementById('cpf').value;
+    index_obj.aTeam_id = document.getElementById('team_id').value;
 
+    var sportlist = document.getElementsByName("sport");
+    
+    if(sportlist[0].checked){
+        index_obj.aSport.push(sportlist[0].id);
+    }
 
+    if(sportlist[1].checked){
+        index_obj.aSport.push(sportlist[1].id);
+    }
 
+    if(sportlist[2].checked){
+        index_obj.aSport.push(sportlist[2].id);
+    }
+
+    if(sportlist[3].checked){
+        index_obj.aSport.push(sportlist[3].id);
+    }
+
+    if(sportlist[4].checked){
+        index_obj.aSport.push(sportlist[4].id);
+    }
+
+    if(sportlist[5].checked){
+        index_obj.aSport.push(sportlist[5].id);
+    }
+
+    if(sportlist[6].checked){
+        index_obj.aSport.push(sportlist[6].id);
+    }
+
+    var vJson = JSON.stringify(index_obj);
+    jresposta.innerHTML = vJson;
 }
